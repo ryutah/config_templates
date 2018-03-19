cb69f885d367fa8eb5fc14803a5db1c2a356cab2
ryutah <ryuta0221@gmail.com>
2018-03-19 13:33:15 +0900

    Update: webpack config

--- webpack/webpack.dev.config.babel.js
+++ webpack/webpack.dev.config.babel.js
@@ -1,11 +1,14 @@
-import config from "./webpack.base.config.babel"
+import config from "./webpack.base.config.babel";
 
-config.devtool = "inline-source-map"
-config.devServer = {
-  inline: true,
-  contentBase: "./public",
-  port: "3000",
-  host: "0.0.0.0",
-}
+const devConfig = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    inline: true,
+    contentBase: "./public",
+    port: "3000",
+    host: "0.0.0.0"
+  }
+};
 
-export default config
+export default Object.assign({}, config, devConfig);
--- webpack/webpack.prod.config.babel.js
+++ webpack/webpack.prod.config.babel.js
@@ -1,18 +1,21 @@
-import webpack from "webpack"
-import config from "./webpack.base.config.babel"
+import UglifyJsPlugin from "uglifyjs-webpack-plugin";
+import config from "./webpack.base.config.babel";
 
-config.plugins = [
-  new webpack.optimize.UglifyJsPlugin({
-    // if need save license after ugilify
-    // output: {
-    //   comments: uglifySaveLicense,
-    // },
-    // if need save comment after ugilify
-    // comments: true,
-    compress: {
-      warnings: false
-    },
-  }),
-]
+const prodConfig = {
+  mode: "production",
+  plugins: [
+    new UglifyJsPlugin({
+      // if need save license after ugilify
+      // output: {
+      //   comments: uglifySaveLicense,
+      // },
+      // if need save comment after ugilify
+      // comments: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
 
-export default config
+export default Object.assign({}, config, prodConfig);
